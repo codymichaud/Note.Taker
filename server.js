5eadd84a133843dc3dbb573a2f2d55ea9f9edba8
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-08 21:10:10 -0800

    added get and post function for api/notes. Now able to post data to db.json. figuring out how to get it to save

--- server.js
+++ server.js
@@ -1,32 +1,58 @@
 const path = require('path')
 const express = require('express');
 const fs = require('fs');
+const { request } = require('http');
+const { response } = require('express');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.resolve(__dirname, './public/index.html'), error => {
+app.get('/', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './public/index.html'), error => {
         if (error) console.error(error);
     });
 });
 
-app.get('/notes', function (req, res) {
-    res.sendFile(path.resolve(__dirname, './public/notes.html'), error => {
+app.get('/notes', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './public/notes.html'), error => {
         if (error) console.error(error);
     });
 });
 
-app.get('/api/notes', function (req, res) {
+app.get('/api/notes', function (request, response) {
     fs.readFile('db/db.json', 'utf-8', function (error, content) {
         let alpha = JSON.parse(content);
-        res.send(alpha);
+        response.send(alpha);
     });
 });
+
+app.post('/api/notes', (request, response) => {
+    fs.readFile('db/db.json', (error, data) => {
+        if (error) throw error;
+
+        let json = JSON.parse(data);
+
+        let noteEntry = {
+            title: request.body.title,
+            text: request.body.text
+        };
+
+        json.push(noteEntry);
+
+        fs.writeFile('db/db.json', JSON.stringify(json, null, 3), (error) => {
+            if (error) throw error;
+            response.send('202');
+
+        });
+    });
+});
+
+
 
 app.listen(PORT, () => {
     console.log('App listening on ' + PORT);
